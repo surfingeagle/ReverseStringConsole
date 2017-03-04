6b35edc59de8cdeec7b103e9dedae0aeae93afac
Keith Noll <keithnoll@outlook.com>
2017-03-04 18:55:57 -0500

    added some error handling for the fun of it. std::exception

--- ReverseStringConsole/ReverseStringConsole.cpp
+++ ReverseStringConsole/ReverseStringConsole.cpp
@@ -4,26 +4,39 @@
 #include "stdafx.h"
 #include <string>
 #include <iostream>
+// #include <stdexcept>
+// using namespace std;
 
 int main(int argc, char * argv[])
 {
-	// Use first argument
-	if (argv[1] != NULL) 
+	try
 	{
-		char *InputString = argv[1];
-		int LastIndex = (int)strlen(InputString) - 1;
-		// swap characters using XOR at first and last index
-		// then traverse to the middle
-		for (int StartIndex = 0; StartIndex < LastIndex; StartIndex++)
+		// test error
+		// throw std::exception("Something Invalid");
+
+		// Use first argument
+		if (argv[1] != NULL)
 		{
-			InputString[StartIndex] = InputString[StartIndex] ^ InputString[LastIndex];
-			InputString[LastIndex] = InputString[LastIndex] ^ InputString[StartIndex];
-			InputString[StartIndex] = InputString[LastIndex] ^ InputString[StartIndex];
-			LastIndex--;
+			char *InputString = argv[1];
+			int LastIndex = (int)strlen(InputString) - 1;
+			// swap characters using XOR at first and last index
+			// then traverse to the middle
+			for (int StartIndex = 0; StartIndex < LastIndex; StartIndex++)
+			{
+				InputString[StartIndex] = InputString[StartIndex] ^ InputString[LastIndex];
+				InputString[LastIndex] = InputString[LastIndex] ^ InputString[StartIndex];
+				InputString[StartIndex] = InputString[LastIndex] ^ InputString[StartIndex];
+				LastIndex--;
+			}
+			// output reversed string
+			// printf("%s\n", InputString);
+			std::cout << InputString << std::endl;
 		}
-		// output reversed string
-		// printf("%s\n", InputString);
-		std::cout << InputString << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		std::cerr << "Type: " << typeid(e).name() << std::endl;
 	}
 	return 0;
 }
